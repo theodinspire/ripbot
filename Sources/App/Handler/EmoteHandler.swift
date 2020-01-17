6402ee482e268f745f41bade51fd61f2551ea9ab
Eric T Cormack <bobryuu@gmail.com>
2020-01-17 15:30:43 -0600

    Add clearer error handing information

--- Sources/App/Handler/EmoteHandler.swift
+++ Sources/App/Handler/EmoteHandler.swift
@@ -8,56 +8,64 @@
 import Foundation
 import Regex
 
+import Console
+
 class EmoteHandler : Handler {
-    static let address = "https://slack.com/api/reactions.add"
-//        "https://www.mocky.io/v2/5185415ba171ea3a00704eed"
+	static let address = "https://slack.com/api/reactions.add"
+	//        "https://www.mocky.io/v2/5185415ba171ea3a00704eed"
 
-    let pattern: Regex
-    let emote: String
+	let pattern: Regex
+	let emote: String
 
-    init(_ emote: String, on pattern: StaticString) {
-        self.emote = emote
-        self.pattern = Regex(pattern)
-    }
+	init(_ emote: String, on pattern: StaticString) {
+		self.emote = emote
+		self.pattern = Regex(pattern)
+	}
 
-    func act(on event: Event) {
-        guard event.allTexts.allSatisfy(pattern.matches(_:)) else {
-            return
-        }
+	func act(on event: Event) {
+		guard event.allTexts.allSatisfy(pattern.matches(_:)) else {
+			return
+		}
 
-        guard let channel = event.channel,
-            let url = URL(string: EmoteHandler.address) else {
-                return
-        }
+		guard let channel = event.channel,
+			let url = URL(string: Self.address) else {
+				return
+		}
 
-        let reaction = Reaction(
-            channel: channel, timestamp: event.event_ts, name: emote)
-        var request = URLRequest(url: url)
+		let reaction = Reaction(
+			channel: channel, timestamp: event.event_ts, name: emote)
+		var request = URLRequest(url: url)
 
-        request.setValue(
-            "Bearer \(KeyChain.botToken)", forHTTPHeaderField: "Authorization")
+		request.setValue(
+			"Bearer \(KeyChain.botToken)", forHTTPHeaderField: "Authorization")
 
-        do {
-            request.httpBody = try JSONEncoder().encode(reaction)
-            URLSession.shared.dataTask(
-                with: request,
-                completionHandler: { data, response, error in
-                    if let error = error {
-                        print("Error:", error)
-                        return
-                    }
+		do {
+			request.httpBody = try JSONEncoder().encode(reaction)
+			URLSession.shared.dataTask(
+				with: request,
+				completionHandler: { data, response, error in
+					let terminal = Terminal()
 
-                    guard let data = data,
-                        let response = response as? HTTPURLResponse else {
-                        print ("Error parsing data or response")
-                        return
-                    }
+					if let error = error {
+						terminal.output("Error: ".consoleText(color: .red) + "\(error)".consoleText())
+						return
+					}
 
-                    print(String(data: data, encoding: .utf8) ?? "What string")
-                    print("Status code:", response.statusCode)
-                    }).resume()
-        } catch {
-            print(error)
-        }
-    }
+					guard let data = data,
+						let response = response as? HTTPURLResponse else {
+							terminal.error("Error parsing data or response")
+							return
+					}
+
+					guard response.statusCode == 200 else {
+						terminal.error("Invalid response code: \(response.statusCode)")
+						return
+					}
+
+					terminal.output(String(data: data, encoding: .utf8)?.consoleText() ?? "What string".consoleText())
+			}).resume()
+		} catch {
+			print(error)
+		}
+	}
 }
