ce1bdfadb388fc22d87988ea60ebe63b70c96abf
Eric T Cormack <bobryuu@gmail.com>
2020-01-17 16:06:02 -0600

    Restore space indenting on the controller

--- Sources/App/Controllers/RipBotController.swift
+++ Sources/App/Controllers/RipBotController.swift
@@ -11,46 +11,46 @@ import Dispatch
 import Regex
 
 class RipBotController {
-	private let matchTable: [Regex: String] = [
-		Regex("Build .*\\d+.* failed") : "rip",
-		Regex("sandwich", options: .ignoreCase) : "sandwich",
-		Regex("thank(s| you)", options: .ignoreCase) : "heart",
-		Regex("ripbot", options: .ignoreCase) : "robot_face",
-	]
+    private let matchTable: [Regex: String] = [
+        Regex("Build .*\\d+.* failed") : "rip",
+        Regex("sandwich", options: .ignoreCase) : "sandwich",
+        Regex("thank(s| you)", options: .ignoreCase) : "heart",
+        Regex("ripbot", options: .ignoreCase) : "robot_face",
+    ]
 
-	private let handlers: [Handler] = [
-		EmoteHandler("rip", on: "Build .*\\d+.* failed"),
-		EmoteHandler("sandwich", on: "sandwich"),
-		EmoteHandler("heart", on: "thank(s| you)"),
-		EmoteHandler("robot_face", on: "ripbot")
-	]
+    private let handlers: [Handler] = [
+        EmoteHandler("rip", on: "Build .*\\d+.* failed"),
+        EmoteHandler("sandwich", on: "sandwich"),
+        EmoteHandler("heart", on: "thank(s| you)"),
+        EmoteHandler("robot_face", on: "ripbot")
+    ]
 
-	func handle(_ request: Request, from slack: Slack) throws -> HTTPResponse {
-		guard slack.isCanonical else {
-			throw Abort(.forbidden)
-		}
+    func handle(_ request: Request, from slack: Slack) throws -> HTTPResponse {
+        guard slack.isCanonical else {
+            throw Abort(.forbidden)
+        }
 
-		switch slack.type {
-		case .challenge:
-			guard let challenge = slack.challenge else {
-				throw Abort(.badRequest)
-			}
-			return HTTPResponse(status: .ok, body: challenge)
-		case .event:
-			let wrapper = try request.content.decode(EventWrapper.self).wait()
-			act(upon: wrapper.event)
-		}
+        switch slack.type {
+        case .challenge:
+            guard let challenge = slack.challenge else {
+                throw Abort(.badRequest)
+            }
+            return HTTPResponse(status: .ok, body: challenge)
+        case .event:
+            let wrapper = try request.content.decode(EventWrapper.self).wait()
+            act(upon: wrapper.event)
+        }
 
-		return HTTPResponse(status: .ok)
-	}
+        return HTTPResponse(status: .ok)
+    }
 
-	func act(upon event: Event) {
-		print("Handling response for event \(event.event_ts)")
+    func act(upon event: Event) {
+        print("Handling response for event \(event.event_ts)")
 
-		for handler in handlers {
-			DispatchQueue.global(qos: .background).async {
-				handler.act(on: event)
-			}
-		}
-	}
+        for handler in handlers {
+            DispatchQueue.global(qos: .background).async {
+                handler.act(on: event)
+            }
+        }
+    }
 }
