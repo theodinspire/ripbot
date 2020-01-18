24bd4be4f1e4d8189bec7d76398a01f0b77be247
Eric T Cormack <bobryuu@gmail.com>
2020-01-17 20:22:08 -0600

    Use POST method for sending response. Remove debug output.

--- Sources/App/Handler/EmoteHandler.swift
+++ Sources/App/Handler/EmoteHandler.swift
@@ -23,8 +23,6 @@ class EmoteHandler : Handler {
 	}
 
 	func act(on event: Event) {
-		let terminal = Terminal()
-
 		guard event.allTexts.contains(where: pattern.matches(_:)) else {
 			return
 		}
@@ -36,59 +34,20 @@ class EmoteHandler : Handler {
 
 		let reaction = Reaction(
 			channel: channel, timestamp: event.event_ts, name: emote)
-		var request = URLRequest(url: url)
 
+		var request = URLRequest(url: url)
+		request.httpMethod = "POST"
 		request.setValue(
 			"Bearer \(KeyChain.botToken)", forHTTPHeaderField: "Authorization")
 
 		do {
-			terminal.output("Sending reaction \(emote).".consoleText(color: .yellow))
-			let body = try JSONEncoder().encode(reaction)
-			request.httpBody = body
-
-			let bodyString = String(data: body, encoding: .utf8)
-				?? "Could not parse body."
-			terminal.output((request.allHTTPHeaderFields?.debugDescription
-				?? "No headers found").consoleText(color: .brightMagenta))
-			terminal.output(bodyString.consoleText(color: .brightYellow))
+			request.httpBody = try JSONEncoder().encode(reaction)
 
 			URLSession.shared.dataTask(
 				with: request,
-				completionHandler: { data, response, error in
-					let terminal = Terminal()
-					print("Request sent.")
-
-					if let error = error {
-						terminal.output("Error: ".consoleText(color: .red) + "\(error)".consoleText())
-						return
-					}
-
-					guard let data = data,
-						let response = response as? HTTPURLResponse else {
-							terminal.error("Error parsing data or response")
-							return
-					}
-
-					guard response.statusCode == 200 else {
-						terminal.error("Invalid response code: \(response.statusCode)")
-
-						let content =
-							String(data: data, encoding: .utf8)
-								?? "What string"
-						let lines = content.components(separatedBy: "\n")
-						let start = max(0, lines.endIndex - 10)
-
-						for line in lines[start..<lines.endIndex].map({ $0.consoleText(color: .green) }) {
-							terminal.output(line)
-						}
-
-						return
-					}
-
-					terminal.output(String(data: data, encoding: .utf8)?.consoleText() ?? "What string".consoleText())
-			}).resume()
+				completionHandler: { _,_,_ in }).resume()
 		} catch {
-			print(error)
+			Terminal().error("Error making reaction \(emote) to message \(event.event_ts): \(error.localizedDescription)")
 		}
 	}
 }
